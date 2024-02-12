6f9d88d485de87adf572797879798fe15b80a2fb
Dylan <dylanryan431@gmail.com>
2024-02-12 12:06:23 -0400

    Set up blend tree

--- Assets/CharacterController2D.cs
+++ Assets/CharacterController2D.cs
@@ -7,14 +7,26 @@ public class CharacterController2D : MonoBehaviour
     public float moveSpeed = 5f;
     [SerializeField] Rigidbody2D rb;
     Vector2 movement;
+    Animator animator;
     private void Awake()
     {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
     private void Update()
     {
         movement.y = Input.GetAxisRaw("Vertical");
-        movement.x = Input.GetAxisRaw("Horizontal");        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        if(movement != Vector2.zero)
+        {
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+        animator.SetFloat("motionY", movement.y);
+        animator.SetFloat("motionX", movement.x);
     }
 
     private void FixedUpdate()
